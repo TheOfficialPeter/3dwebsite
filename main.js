3acdbf3d283cdda139cad30091ae8bdab8b7103a
TheOfficialPeter <foxwire121@hotmail.com>
2022-06-06 18:45:11 +0200

    Triangle

--- main.js
+++ main.js
@@ -1,72 +1,46 @@
 function main() {
-	const canvas = document.querySelector("#glCanvas");
-	const gl = canvas.getContext("webgl");
+	/* Step1: Prepare the canvas and get WebGL context */
 
-	if (gl===null){
-		alert("Browser doesn't support webgl");
-		return;
-	}
-
-	gl.clearColor(0,0,0,1);
-	gl.clear(gl.COLOR_BUFFER_BIT);
+	var canvas = document.getElementById('glCanvas');
+	var gl = canvas.getContext('webgl');
 	
-	var vc = `
-			attribute vec4 a_position;
-
-			void main() {
-				gl_Position = a_position;
-			}
-		`;
+	var vertices = [-0.5, 0.5, -0.5, -0.5, 0.0, -0.5,];
 	
-	var fc = `
-			precision mediump float;
+	var vertex_buffer = gl.createBuffer();
 
-			void main() {
-				gl_FragColor = vec4(1, 0, 0.5, 1);
-			}
-		`;
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+	gl.bindBuffer(gl.ARRAY_BUFFER, null);
 
-	vcShader = gl.createShader(gl.VERTEX_SHADER);
-	fcShader = gl.createShader(gl.FRAGMENT_SHADER);
+	var vertCode =
+	'attribute vec2 coordinates;' + 
+	'void main(void) {' + ' gl_Position = vec4(coordinates,0, 1);' + '}';
 
-	gl.shaderSource(vcShader, vc);
-	gl.shaderSource(fcShader, fc);
-	
-	gl.compileShader(vcShader);
-	console.log(gl.getShaderParameter(vcShader, gl.COMPILE_STATUS));
-	gl.compileShader(fcShader);
-	console.log(gl.getShaderParameter(fcShader, gl.COMPILE_STATUS));
+	var vertShader = gl.createShader(gl.VERTEX_SHADER);
+	gl.shaderSource(vertShader, vertCode);
+	gl.compileShader(vertShader);
 
-	const shaderProgram = gl.createProgram();
-	gl.attachShader(shaderProgram, vcShader);
-	gl.attachShader(shaderProgram, fcShader);
+	var fragCode = 'void main(void) {' + 'gl_FragColor = vec4(100.0, 0.0, 0.0, .75);' + '}';
+
+	var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
+	gl.shaderSource(fragShader, fragCode);
+	gl.compileShader(fragShader);
+
+	var shaderProgram = gl.createProgram();
+	gl.attachShader(shaderProgram, vertShader); 
+	gl.attachShader(shaderProgram, fragShader);
+
 	gl.linkProgram(shaderProgram);
 
-	if (!gl.getProgramParameter(shaderProgram, gl.LINK_STATUS)){
-		alert("failed linking the 2 shaders");
-		gl.deleteProgram(shaderProgram);
-		return null;
-	}
-
-	gl.validateProgram(shaderProgram);
-	console.log(gl.getProgramParameter(shaderProgram, gl.LINK_STATUS));
-	
-	var triangleVerts = 
-	[	0, 0,
-		0, 0.5,
-		0.7, 0
-	];
-	
-	var pBuffer = gl.createBuffer();
-	gl.bindBuffer(gl.ARRAY_BUFFER, pBuffer);
-	gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVerts), gl.STATIC_DRAW);
-
-	var pLocation = gl.getAttribLocation(shaderProgram, "a_position");
-	
-	gl.vertexAttribPointer(pLocation, 2, gl.FLOAT, gl.FALSE, 5* Float32Array.BYTES_PER_ELEMENT, 0);
-	gl.enableVertexAttribArray(pLocation);
-
 	gl.useProgram(shaderProgram);
+
+	gl.bindBuffer(gl.ARRAY_BUFFER, vertex_buffer);
+
+	var coord = gl.getAttribLocation(shaderProgram, "coordinates");
+	gl.vertexAttribPointer(coord, 2, gl.FLOAT, false, 0, 0);
+	gl.enableVertexAttribArray(coord);
+	
+	gl.viewport(0,0,canvas.width,canvas.height);
 	gl.drawArrays(gl.TRIANGLES, 0, 3);
 }
 
