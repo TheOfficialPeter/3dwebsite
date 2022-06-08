9e3b41fb254432ef2edf4bace11c2a35b767a828
TheOfficialPeter <foxwire121@hotmail.com>
2022-06-08 20:47:59 +0200

    idk whats wrong

--- main.js
+++ main.js
@@ -1,3 +1,67 @@
 function load() {
+    var canv = document.querySelector("#canvas");
+    var gl = canv.getContext("webgl");
+
+    if (!gl){
+        console.log("Not supported. Trying experminental version");
+        gl = canv.getContext("experimental-webgl");
+    }
+
+    if (!gl){
+        console.log("Webgl not supported on this browser");
+    }
+
+    var vertex_text = 
+    `attribute vec4 position;
     
+    void main(){
+        gl_Position = position;
+    }`;
+
+    var fragment_text = 
+    `precision mediump float;
+    
+    void main(){
+        gl_FragColor = vec4(255, 0, 0, 1);
+    }`;
+
+    var vertices = [
+        -0.5, 0.5,
+        -0.5, -0.5,
+        0.0, -0.5,
+    ];
+
+    var vBuff = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuff);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+    gl.bindBuffer(gl.ARRAY_BUFFER, null);
+
+    var vertex_shader = gl.createShader(gl.VERTEX_SHADER);
+    gl.shaderSource(vertex_shader, vertex_text);
+    gl.compileShader(vertex_shader);
+    
+    var fragment_shader = gl.createShader(gl.FRAGMENT_SHADER);
+    gl.shaderSource(fragment_shader, fragment_text);
+    gl.compileShader(fragment_shader);
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vertex_shader);
+    gl.attachShader(program, fragment_shader);
+
+    gl.linkProgram(program);
+    gl.useProgram(program);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, vBuff);
+
+    var origin = gl.getAttribLocation(program, "position");
+    gl.vertexAttribPointer(origin, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(origin);
+
+    gl.clearColor(0,0,0,1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    
+    gl.viewport(0,0,canv.width,canv.height);
+    gl.drawArrays(gl.TRIANGLE, 0, 3);
 }
+
+window.onload = load();
